4b919054c2303e67b20cb9dff988de1eb6be3815
Florian Hardtke <tYrrrrio@gmail.com>
2013-05-08 15:58:41 +0200

    jQuery

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ OMRails::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
-   root :to => 'pages#home'
+   root :to => 'pins#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
